6749c9e069053c662c2404d52462114afe6014e1
DNepovim <jsem@dominikblaha.cz>
2017-06-18 08:57:55 +0200

    Add units to frontpage

--- theme/front-page.php
+++ theme/front-page.php
@@ -6,4 +6,6 @@ $events = get_posts(array('post_type' => 'event', 'numberposts' => 5, 'order' =>
 
 $news = get_posts(array('post_type' => 'new', 'numberposts' => 3, 'order' => 'ASC'));
 
-view(['events' => $events, 'news' => $news]);
+$units = get_posts(array('post_type' => 'unit', 'numberposts' => -1, 'order' => 'ASC'));
+
+view(['events' => $events, 'news' => $news, 'units' => $units]);
